88ad9f7a3696b47d9110c20208babd923da17cc0
Jennifer <buenojenniferx3@gmail.com>
2024-05-02 22:53:00 -0400

    Your commit message here

---
+++ src/main/java/com/ps/Transactionsclass.java
@@ -0,0 +1,67 @@
+package com.ps;
+
+ class Transaction {
+    private String date;
+    private String time;
+    private String description;
+
+     public String getDate() {
+         return date;
+     }
+
+     public void setDate(String date) {
+         this.date = date;
+     }
+
+     public String getTime() {
+         return time;
+     }
+
+     public void setTime(String time) {
+         this.time = time;
+     }
+
+     public String getDescription() {
+         return description;
+     }
+
+     public void setDescription(String description) {
+         this.description = description;
+     }
+
+     public String getVendor() {
+         return vendor;
+     }
+
+     public void setVendor(String vendor) {
+         this.vendor = vendor;
+     }
+
+     public double getAmount() {
+         return amount;
+     }
+
+     public void setAmount(double amount) {
+         this.amount = amount;
+     }
+
+     private String vendor;
+    private double amount;
+
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String toCSV() {
+        return String.format("%s|%s|%s|%s|%.2f", date, time, description, vendor, amount);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\t%s\t%s\t%s\t%.2f", date, time, description, vendor, amount);
+    }
+}
--- src/main/java/com/ps/financialTrackerCLI.java
+++ src/main/java/com/ps/financialTrackerCLI.java
@@ -87,28 +87,5 @@ public class financialTrackerCLI {
         }
     }
 
-    private static class Transaction {
-        private String date;
-        private String time;
-        private String description;
-        private String vendor;
-        private double amount;
 
-        public Transaction(String date, String time, String description, String vendor, double amount) {
-            this.date = date;
-            this.time = time;
-            this.description = description;
-            this.vendor = vendor;
-            this.amount = amount;
-        }
-
-        public String toCSV() {
-            return String.format("%s|%s|%s|%s|%.2f", date, time, description, vendor, amount);
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%s\t%s\t%s\t%s\t%.2f", date, time, description, vendor, amount);
-        }
-    }
 }
